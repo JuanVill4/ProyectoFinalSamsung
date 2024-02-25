bef1bcd9b5b0929492d9726c27caaa901bba0049
JuanVill4 <juan.villanuevaf@udem.edu>
2024-02-24 19:03:21 -0600

    Update OLED display code to show a resized image

--- Code/Display/oled_colors.py
+++ Code/Display/oled_colors.py
@@ -1,16 +1,22 @@
-
 from luma.core.interface.serial import i2c
-from luma.core.render import canvas
 from luma.oled.device import sh1106
 from PIL import Image
+
+# Inicialización del bus I2C y dirección del dispositivo OLED
+serial = i2c(port=1, address=0x3C)
+
+# Inicialización del dispositivo OLED con el controlador SH1106
+device = sh1106(serial, rotate=0)
+
 # Ruta de la imagen PBM
-image_path = "/home/juanv/Documents/ProyectoSamsung/ProyectoFinalSamsungEquipo10/Imagenes/heart.pbm"
+image_path = 'Imagenes/heart.pbm'  # Asegúrate de cambiar esto por la ruta real de tu imagen
 
-# Cargar la imagen desde un archivo PBM
-with open(image_path, "rb") as f:
-    image_data = f.read()
+# Cargar la imagen PBM
+image = Image.open(image_path)
 
-# Mostrar la imagen en el display
-with canvas(sh1106) as draw:
-    draw.bitmap((0, 0), image_data, fill="white")
+# Redimensionar la imagen al tamaño de la pantalla OLED si es necesario
+# Esto asume que la resolución del dispositivo es 128x64. Ajusta según tu dispositivo.
+image_resized = image.resize(device.size)
 
+# Mostrar la imagen en el dispositivo OLED
+device.display(image_resized)
