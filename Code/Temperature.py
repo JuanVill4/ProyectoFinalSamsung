abee7cb45561f37012e29c8b156c3f5e7d42b93a
JuanVill4 <juan.villanuevaf@udem.edu>
2024-03-02 00:13:46 -0600

    Update temperature and pulsioximeter code

--- Code/Temperature.py
+++ Code/Temperature.py
@@ -1,28 +1,24 @@
-import adafruit_dht
-import board
+import Adafruit_DHT
 import time
 
 # Inicializa el sensor DHT11.
 # Reemplaza 'D4' con el pin al que está conectado tu sensor DHT11.
 # Por ejemplo, para GPIO4 en Raspberry Pi, usarías board.D4.
-sensor = adafruit_dht.DHT11(board.D4)
+sensor = Adafruit_DHT.DHT11
+pin = '13'
 
 while True:
-    try:
-        # Lee la humedad y la temperatura del sensor.
-        temperature = sensor.temperature
-        humidity = sensor.humidity
-        
-        # Imprime los valores leídos.
-        print(f"Temperatura: {temperature}°C  Humedad: {humidity}%")
+    # Intenta obtener la lectura del sensor.
+    # La función read_retry intenta obtener lecturas del sensor varias veces (hasta 15 por defecto)
+    # para minimizar los errores de lectura.
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-    except RuntimeError as error:
-        # Errores que suelen suceder de forma rutinaria, como lecturas fallidas debido a temporización.
-        print(error.args[0])
-        
-    except Exception as error:
-        sensor.exit()
-        raise error
-
-    # Espera un poco antes de leer nuevamente
-    time.sleep(2.0)
+    if humidity is not None and temperature is not None:
+        # Imprime los resultados.
+        print('Temperatura={0:0.1f}*C  Humedad={1:0.1f}%'.format(temperature, humidity))
+    else:
+        # Si no se pudo obtener la lectura, imprime un mensaje de error.
+        print('Fallo la lectura del sensor. Intente de nuevo!')
+    
+    # Espera 2 segundos antes de la próxima lectura.
+    time.sleep(2)
--- Code/pulsioximeter.py
+++ Code/pulsioximeter.py
@@ -1,35 +1,90 @@
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import sh1106
+from PIL import ImageFont
+import sys
+from pathlib import Path
 import time
-from Pulsioximeter import max30102, hrcalc
+from gpiozero import LED, Button
 
+# Agrega la carpeta Oxigen al sys.path
+oxigen_path = Path(__file__).resolve().parent / 'Oxigen'
+sys.path.append(str(oxigen_path))
+from hrmonitor import HeartRateMonitor
 
 # Inicialización del bus I2C y dirección del dispositivo OLED
 serial = i2c(port=1, address=0x3C)
 
 # Inicialización del dispositivo OLED con el controlador SH1106
 device = sh1106(serial, rotate=0)
+# Configuración de la fuente así como cálculo para posición centrada
+font_path = "ProyectoFinalSamsungEquipo10/Code/templates/ALBA____.TTF"
+font = ImageFont.truetype(font_path, 18)
+with canvas(device) as draw:
+        ancho_texto, alto_texto = draw.textsize("Pulsioximetro", font=font)
+        ancho_display, alto_display = device.width, device.height
+        x = (ancho_display - ancho_texto) / 2
 
-# Inicialización del sensor MAX30102
-m = max30102.MAX30102()
+        ancho_texto1, alto_texto1 = draw.textsize("Dedo no detectado")
+        x1 = (ancho_display - ancho_texto1) / 2
+# Configuración de LEDs y botones
+led_verde = LED(23)  # Asume que el LED verde está conectado al pin GPIO 23
+led_rojo = LED(18)  # Asume que el LED rojo está conectado al pin GPIO 18
+boton = Button(12)  # Asume que el botón está conectado al pin GPIO 2
 
-# Función para obtener la frecuencia cardíaca y la saturación de oxígeno del sensor MAX30102
-def get_hr_and_spo2():
-    red, ir = m.read_sequential()
-    hr, hrb, sp, spb = hrcalc.calc_hr_and_spo2(ir, red)
-    return hr, sp
+# Estado del sistema
+sistema_encendido = False
 
-# Bucle principal para actualizar la pantalla cada 10 segundos
-while True:
-    # Limpiar la pantalla
-    with canvas(device) as draw:
-        # Obtener la frecuencia cardíaca y la saturación de oxígeno
-        hr, sp = get_hr_and_spo2()
-        
-        # Mostrar la frecuencia cardíaca y la saturación de oxígeno
-        draw.text((0, 0), f"Heart Rate: {hr} bpm", fill="white")
-        draw.text((0, 16), f"SpO2: {sp}%", fill="white")
-        
-    # Esperar 10 segundos antes de la próxima actualización
-    time.sleep(1)
+# Función para encender y apagar el sistema
+def toggle_sistema():
+    global sistema_encendido
+    sistema_encendido = not sistema_encendido
+    if sistema_encendido:
+        print("Sistema encendido")
+        device.show()  # Reactiva la pantalla OLED
+
+    else:
+        print("Sistema apagado")
+        device.hide()  # Apaga la pantalla OLED
+        led_verde.off()  # Asegura que el LED verde se apague cuando el sistema se apague
+        led_rojo.off()  # Enciende el LED rojo para indicar que el sistema está apagado
+
+boton.when_pressed = toggle_sistema
+
+# Función para mostrar los datos del sensor en el OLED
+def display_sensor_data(hrm):
+    if sistema_encendido:
+        with canvas(device) as draw:
+            draw.text((x, 0), "Pulsioxímetro", font=font, fill="white")
+            if hrm.bpm > 0:      
+                draw.text((10, 25), f"Heart Rate: {int(hrm.bpm)} BPM", fill="white")
+                draw.text((10, 40), f"SpO2: {int(hrm.spo2)}%", fill="white")
+                led_verde.on()
+                led_rojo.off()
+            else:
+                draw.text((x1, 35), "Dedo no detectado", fill="white")
+                led_verde.off()
+                led_rojo.on()
+
+def monitor_heart_rate_and_spo2():
+    hrm = HeartRateMonitor(print_raw=False, print_result=False)
+    hrm.start_sensor()
+
+    try:
+        print("Monitoreo de la frecuencia cardíaca y oxigenación iniciado. Presiona CTRL+C para detener. Presiona el botón para iniciar/detener el monitoreo.")
+        while True:
+            if sistema_encendido:
+                display_sensor_data(hrm)
+            else:
+                led_verde.off()  # Apaga el LED verde
+                led_rojo.off()  # Apaga el LED rojo
+            time.sleep(0.5)  # Espera medio segundo antes de leer el siguiente valor
+    except KeyboardInterrupt:
+        print("Deteniendo el monitoreo...")
+    finally:
+        hrm.stop_sensor()
+        led_verde.off()  # Apaga el LED verde
+        led_rojo.off()  # Apaga el LED rojo
+
+if __name__ == "__main__":
+    monitor_heart_rate_and_spo2()
